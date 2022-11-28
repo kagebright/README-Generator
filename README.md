495ab284c6e49f4b1260962fdf3b22a2ef2263d3
Bright Abety <bright@Brights-MacBook-Pro.local>
2022-11-28 18:23:00 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # README-Generator
+This application creates a professional high-quality REEADME file. 
+
+Technologies used: JavaScript and Node.js
+
+Challenge: In this week,s challenge we will using node.js and inquirer in JavaScript to create a professional README file by using a command-line application.
+
+## Links:
+
+### Website:
+
+### GitHub: https://github.com/kagebright/README-Generator
---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = reauire("fs");
+const inquirer = require('inquirer');
+const util = require("util");
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: '',
+      name: 'username',
+    },
+    {
+      type: 'password',
+      message: 'What is your password?',
+      name: 'password',
+    },
+    {
+      type: 'password',
+      message: 'Re-enter password to confirm:',
+      name: 'confirm',
+    },
+  ])
+  .then((response) =>
+    response.confirm === response.password
+      ? console.log('Success!')
+      : console.log('You forgot your password already?!')
+  );
