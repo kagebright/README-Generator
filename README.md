9f589fe9924fce9da719de71d39fdf28347f9b9e
Bright Abety <bright@Brights-MacBook-Pro.local>
2022-12-04 21:24:04 -0500

    index.js updates

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ This application creates a professional high-quality README file.
 
 Technologies used: JavaScript and Node.js
 
-Challenge: In this week,s challenge we will using node.js and inquirer in JavaScript to create a professional README file by using a command-line application.
+Challenge: In this week's challenge we will using node.js and inquirer in JavaScript to create a professional README file by using a command-line application.
 
 ## Links:
 
--- index.js
+++ index.js
@@ -1,27 +1,60 @@
-const fs = reauire("fs");
+const fs = require("fs");
 const inquirer = require('inquirer');
 const util = require("util");
+
+//writing the README file
+const writeFileAsync = util.promisify(fs.writefile);
 
 inquirer
   .prompt([
     {
       type: 'input',
-      message: '',
-      name: 'username',
+      message: 'What is the title of the README',
+      name: 'title',
     },
     {
-      type: 'password',
-      message: 'What is your password?',
-      name: 'password',
+      type: 'input',
+      message: 'What is the description of the README.md?',
+      name: 'Description',
     },
     {
-      type: 'password',
-      message: 'Re-enter password to confirm:',
-      name: 'confirm',
+      type: 'input',
+      message: 'What is included in the README?',
+      name: 'table of contents',
+    },
+    {
+      type: 'input',
+      message: 'What is the installation process?',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'What will the app be used for?',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'What license is used for this application?',
+      name: 'license',
+    },
+    {
+      type: 'input',
+      message: 'Who will be working on this application with you?',
+      name: 'contributing',
+    },
+    {
+      type: 'input',
+      message: 'What tests are you running on your application?',
+      name: 'tests',
+    },
+    {
+      type: 'input',
+      message: 'What is included in the README?',
+      name: 'questions',
     },
   ])
   .then((response) =>
-    response.confirm === response.password
+    response.confirm === response.input
       ? console.log('Success!')
-      : console.log('You forgot your password already?!')
+      : console.log('You forgot your input already?!')
   );
